0e96ba5d77e8163e403634402963c20548673192
ZoltanKollar1987 <62061024+ZoltanKollar1987@users.noreply.github.com>
2020-03-11 16:51:06 +0100

    Level Create

---
+++ DefendTheBase!/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] tilePrefabs;
+
+    public float TileSize
+    {
+        get {return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateLevel()
+    {
+
+        string[] mapData = ReadLevelText();
+
+        int mapX = mapData[0].ToCharArray().Length;
+        int mapY = mapData.Length;
+
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+
+        for (int y = 0; y < mapY; y++)
+        {
+            char[] newTiles = mapData[y].ToCharArray();
+
+            for (int x = 0; x < mapX; x++)
+            {
+                PlaceTile(newTiles[x].ToString(),x,y,worldStart);
+            }
+        }
+
+    }
+
+    private void PlaceTile(string tileType,int x, int y,Vector3 worldStart)
+    {
+        int tileIndex = int.Parse(tileType);
+
+        GameObject newTile = Instantiate(tilePrefabs[tileIndex]);
+
+        newTile.transform.position = new Vector3(worldStart.x +( TileSize * x),worldStart.y - ( TileSize * y), 0);
+    }
+
+    private string[] ReadLevelText()
+    {
+        TextAsset bindData = Resources.Load("Level") as TextAsset;
+        string data = bindData.text.Replace(Environment.NewLine, string.Empty);
+        return data.Split('-');
+    }
+}
